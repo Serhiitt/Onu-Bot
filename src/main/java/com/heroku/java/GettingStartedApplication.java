1f83eb569456a73a47e3d53e124a7647015099bf
Serhiitt <sergey.telezhnikov2517@gmail.com>
2023-04-13 22:35:08 +0300

    make it better

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -48,7 +52,15 @@ public class GettingStartedApplication {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TelegramApiException {
         SpringApplication.run(GettingStartedApplication.class, args);
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(new SimpleEchoBot());
+            System.out.println("Бот запущений");
+        } catch (TelegramApiRequestException e) {
+            e.getErrorCode();
+        }
     }
 }
+
---
+++ src/main/java/com/heroku/java/SimpleEchoBot.java
@@ -0,0 +1,132 @@
+package com.heroku.java;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SimpleEchoBot extends TelegramLongPollingBot {
+    public static String GREEN = "\uD83D\uDFE2";
+    public static String YELLOW = "\uD83D\uDFE1";
+    public static String RED = "\uD83D\uDD34";
+
+    ArrayList<String> list = new ArrayList<>();
+    ArrayList<String> online = new ArrayList<>();
+    ArrayList<String> offline = new ArrayList<>();
+    ArrayList<String> down = new ArrayList<>();
+    ArrayList<String> badSignal = new ArrayList<>();
+    ArrayList<String> up = new ArrayList<>();
+    int onuCount;
+
+    public boolean messageSize(){
+        List<String> list1 = new ArrayList<>(Arrays.asList(list.get(0).split("\n")));
+        List<String> list2 = new ArrayList<>(Arrays.asList(list.get(1).split("\n")));
+        if (list1.size() == list2.size()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void sort(){
+        List<String> list1 = new ArrayList<>(Arrays.asList(list.get(0).split("\n")));
+        List<String> list2 = new ArrayList<>(Arrays.asList(list.get(1).split("\n")));
+        onuCount = list1.size();
+
+        for (int i = 0; i < list1.size(); i++){
+            String strBefore = list1.get(i);
+            String strAfter = list2.get(i);
+            if (strBefore.contains(GREEN) && strAfter.contains(GREEN)){
+                online.add(strBefore + "\n");
+            }
+            if (strBefore.contains(GREEN) && strAfter.contains(RED)){
+                down.add(strAfter + "\n");
+            }
+            if (strBefore.contains(RED) && strAfter.contains(RED)){
+                offline.add(strBefore + "\n");
+            }
+            if (strBefore.contains(YELLOW) || strAfter.contains(YELLOW)){
+                badSignal.add(strBefore + "\n");
+            }
+            if (strBefore.contains(RED) && strAfter.contains(GREEN)){
+                up.add(strAfter + "\n");
+            }
+        }
+    }
+
+    public void clear(){
+        list.clear();
+        online.clear();
+        offline.clear();
+        down.clear();
+        badSignal.clear();
+        up.clear();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        String textFromUser = update.getMessage().getText();
+        String line = textFromUser;
+        Long userId = update.getMessage().getChatId();
+        if (line.equals("/start")){
+            SendMessage firstMessage = SendMessage.builder().chatId(userId.toString()).text("Пришліть 2 списка ону, бот порівняє їх").build();
+            try {
+                this.sendApiMethod(firstMessage);
+            } catch (TelegramApiException e ){
+                e.getMessage();
+            }
+        } else {
+            list.add(line);
+
+            if (list.size() == 2 && messageSize() == true){
+                sort();
+                String onlineSize = String.valueOf(online.size());
+                String offlineSize = String.valueOf(offline.size());
+                String badSignalSize = String.valueOf(badSignal.size());
+                String downSize = String.valueOf(down.size());
+                String upSize = String.valueOf(up.size());
+                SendMessage sendSize = SendMessage.builder().chatId(userId.toString()).text("Всього ону: " + onuCount).build();
+                SendMessage sendOnline = SendMessage.builder().chatId(userId.toString()).text("Онлайн: " + onlineSize + "\n" + online.toString().replace(",", "").replace("[", "").replace("]", "")).build();
+                SendMessage sendOffline = SendMessage.builder().chatId(userId.toString()).text("Були оффлайн: " + offlineSize + "\n" + offline.toString().replace(",", "").replace("[", "").replace("]", "")).build();
+                SendMessage sendDown = SendMessage.builder().chatId(userId.toString()).text("Упали: " + downSize + "\n" + down.toString().replace(",", "").replace("[", "").replace("]", "")).build();
+                SendMessage sendBadSignal = SendMessage.builder().chatId(userId.toString()).text("Поганий сигнал: " + badSignalSize + "\n" + badSignal.toString().replace(",", "").replace("[", "").replace("]", "")).build();
+                SendMessage sendUp = SendMessage.builder().chatId(userId.toString()).text("Піднялись: " + upSize + "\n" + up.toString().replace(",", "").replace("[", "").replace("]", "")).build();
+                try {
+                    this.sendApiMethod(sendSize);
+                    this.sendApiMethod(sendOnline);
+                    this.sendApiMethod(sendOffline);
+                    this.sendApiMethod(sendDown);
+                    this.sendApiMethod(sendBadSignal);
+                    this.sendApiMethod(sendUp);
+                } catch (TelegramApiException e) {
+                    e.getMessage();
+                }
+
+                clear();
+
+            } else if (list.size() == 2 && messageSize() == false){
+                SendMessage sendTryAgain = SendMessage.builder().chatId(userId.toString()).text("Кількість ONU не співпадає, спробуйте ще раз").build();
+                clear();
+                try {
+                    this.sendApiMethod(sendTryAgain);
+                } catch (TelegramApiException e){
+                    e.getMessage();
+                }
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "sortBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5261839501:AAEuWqAJL3wxhyaBWA2Yy-ldpZFSi4s50Rk";
+    }
+}
